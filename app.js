32521fe9673de4c1e0ee936f2b36656b94fd0fbc
Hamza Farooq <hamxa.farooq.47@gmail.com>
2023-11-20 20:02:04 +0500

    setting up middlewares

--- app.js
+++ app.js
@@ -3,6 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require("express-session");
+var passport = require("passport");
+var LocalStrategy = require("passport-local").Strategy;
+var multer = require("multer");
+var upload = multer({
+  dest: './uploads'
+})
+var flash = require("connect-flash");
+var mongodb = require("mongodb");
+var mongoose = require("mongoose");
+var db = mongoose.connection;
+var expressMessages = require("express-messages");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -17,7 +29,24 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'))); //use public as static folder
+
+// Handle Sessions
+app.use(session({
+  secret: "secret",
+  saveUninitialized: true,
+  resave: true
+}))
+
+// Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
+app.use(function (req, res, next) {
+  res.locals.messages = expressMessages(req, res);
+  next();
+})
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
