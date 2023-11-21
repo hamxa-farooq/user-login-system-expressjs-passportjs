ada37a8312564933a76099f79f509bbffc19db7b
Hamza Farooq <hamxa.farooq.47@gmail.com>
2023-11-21 14:22:22 +0500

    Add views and layout

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/register', (req, res) =>{
+  res.render('register', {title: 'Register'});
+})
+
+router.get('/login', (req, res) =>{
+  res.render('login', {title: 'Login'});
+})
+
 module.exports = router;
