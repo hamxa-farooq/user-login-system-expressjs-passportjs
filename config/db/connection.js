a0aaf81173b353f67a5cb094094eab00eca3f106
Hamza Farooq <hamxa.farooq.47@gmail.com>
2023-11-25 00:44:49 +0500

    Add chalk to change log colors

--- config/db/connection.js
+++ config/db/connection.js
@@ -1,31 +1,32 @@
 const mongoose = require('mongoose');
+const chalk = require('chalk');
 
 const connection = mongoose.connect('mongodb://localhost/nodeauth');
 
 mongoose.connection.on('connecting', () => {
-  console.log('making initial connection with mongoDB server');
+  console.log(chalk.cyanBright('making initial connection with mongoDB server'));
 });
 
 mongoose.connection.on('connected', () => {
-  console.log('connection successfull with mongoDB server');
+  console.log(chalk.cyanBright('connection successfull with mongoDB server'));
 });
 
 mongoose.connection.on('disconnecting', () => {
-  console.log('disconnecting mongoDB server upon calling connection.close()');
+  console.log(chalk.cyanBright('disconnecting mongoDB server upon calling connection.close()'));
 });
 
 
 mongoose.connection.on('close', () => {
-  console.log('disconnecting mongoDB server upon calling connection.close()');
+  console.log(chalk.cyanBright('disconnecting mongoDB server upon calling connection.close()'));
 });
 
 
 mongoose.connection.on('disconnected', () => {
-  console.log('connection to mongoDB server lost due to database server crash, network connectivity issues or explicitly closing connection');
+  console.log(chalk.cyanBright('connection to mongoDB server lost due to database server crash, network connectivity issues or explicitly closing connection'));
 });
 
 mongoose.connection.on('reconnected', () => {
-  console.log('mongoDB successfully reconnected after losing connection to mongoDB server');
+  console.log(chalk.cyanBright('mongoDB successfully reconnected after losing connection to mongoDB server'));
 });
 
 module.exports = connection;
--- config/mongoStore/storeConfig.js
+++ config/mongoStore/storeConfig.js
@@ -1,4 +1,5 @@
 const MongoStore = require('connect-mongo');
+const chalk = require('chalk');
 
 const store = new MongoStore({
   mongoUrl: 'mongodb://localhost:27017/session-db-testing',
@@ -6,24 +7,23 @@ const store = new MongoStore({
 });
 
 store.on('create', () => {
-  // log(chalk.blue)
-  console.log('A session has been created');
+  console.log(chalk.cyanBright('A session has been created'));
 });
 
 store.on('touch', () => {
-  console.log('User ession has been touched butnot modified');
+  console.log(chalk.cyanBright('User session has been touched butnot modified'));
 });
 
 store.on('update', () => {
-  console.log('A session has been updated');
+  console.log(chalk.cyanBright('User session has been updated'));
 });
 
 store.on('set', () => {
-  console.log('A session has been created or updated');
+  console.log(chalk.cyanBright('A session has been created or updated'));
 });
 
 store.on('destroy', () => {
-  console.log('User session has been destroyed manually');
+  console.log(chalk.cyanBright('User session has been destroyed manually'));
 });
 
 module.exports = store;
